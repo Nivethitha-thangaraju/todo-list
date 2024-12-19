25dca8aeda367731601c0ec2a6b7543c078ebdfa
Nivethitha-thangaraju <nivethithat4@gmail.com>
2024-12-19 21:22:50 +0530

    todo list

---
+++ routes/todoRoutes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const Todo = require('../models/todo');
+const router = express.Router();
+
+// GET all todos
+router.get('/', async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// POST a new todo
+router.post('/', async (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  });
+
+  try {
+    const newTodo = await todo.save();
+    res.status(201).json(newTodo);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// DELETE a todo
+router.delete('/:id', async (req, res) => {
+  try {
+    await Todo.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: 'Todo deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
